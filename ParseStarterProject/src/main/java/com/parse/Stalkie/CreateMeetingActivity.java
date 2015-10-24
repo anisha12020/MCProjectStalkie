3bfdf8460c6ddf8e35b669ea14b42ada38e1b76f
JuhiJetwani <juhi12044@iiitd.ac.in>
2015-10-24 17:32:24 +0530

    Create Meeting activity

--- ParseStarterProject/src/main/java/com/parse/Stalkie/CreateMeetingActivity.java
+++ ParseStarterProject/src/main/java/com/parse/Stalkie/CreateMeetingActivity.java
@@ -1,41 +1,41 @@
 package com.parse.Stalkie;
 
 import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBar.Tab;
-import android.support.v7.app.ActionBarActivity;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class Dashboard extends AppCompatActivity {
+import com.parse.Stalkie.R;
 
+public class CreateMeetingActivity extends Dashboard {
     private Toolbar toolbar;
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+                    Toast.makeText(getApplicationContext(), "Create Meeting Activity",
+                    Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_create_meeting);
+
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard,menu);
+        getMenuInflater().inflate(R.menu.menu_create_meeting, menu);
 
         return true;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -43,29 +43,21 @@ public class Dashboard extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Toast.makeText(getApplicationContext(), "Settings icon",
+//
+//        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_done) {
+            Toast.makeText(getApplicationContext(), "Meeting Created",
                     Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, Dashboard.class);
+            startActivity(intent);
             return true;
         }
-        if (id == R.id.action_add) {
-//            Toast.makeText(getApplicationContext(), "Add icon",
-//                    Toast.LENGTH_SHORT).show();
-            supportInvalidateOptionsMenu();
-
-            Intent intent = new Intent(Dashboard.this, CreateMeetingActivity.class);
+        if (id == R.id.action_back) {
+            Intent intent = new Intent(this, Dashboard.class);
             startActivity(intent);
-
+            return true;
         }
-        return super.onOptionsItemSelected(item);
-    }
+      return super.onOptionsItemSelected(item);
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu){
-//        menu.removeItem(R.id.action_add);
-//        menu.removeItem(R.id.action_settings);
-        return true;
     }
 }
--- ParseStarterProject/src/main/java/com/parse/Stalkie/DashboardActivity.java
+++
@@ -1,9 +0,0 @@
-package com.parse.Stalkie;
-
-import android.app.Activity;
-
-/**
- * Created by Anisha on 23-10-2015.
- */
-public class DashboardActivity extends Activity {
-}
